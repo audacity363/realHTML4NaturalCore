4b2065b35ce71ce3091b33df0354655969923cd5
Tom Engemann <tom@engemann.me>
2020-03-12 18:02:38 +0100

    Removed missing values in logging

--- libs/rh4n_logging/src/rh4n_logging.c
+++ libs/rh4n_logging/src/rh4n_logging.c
@@ -102,7 +102,7 @@ void rh4n_log(RH4nLogrule *rule, int level,
     pid = getpid();
 
     if(rule->level == RH4N_DEVELOP) {
-        snprintf(logprefix, MAX_PREFIX_LEN, "%s.%06ld [%-8s->%-8s@%d=>%d] %-7s (%s@%s:%ld) - ", datetime_buff, millis.tv_usec, 
+        snprintf(logprefix, MAX_PREFIX_LEN, "%s.%06ld [%-8s->%-8s@%d] %-7s (%s@%s:%ld) - ", datetime_buff, millis.tv_usec, 
             rule->nat_library, rule->nat_program, pid, level_str, func, file, line);
     } else {
         snprintf(logprefix, MAX_PREFIX_LEN, "%s.%06ld [%-8s->%-8s@%d] %-7s - ", datetime_buff, millis.tv_usec, 
